b384e07f54220215d4b3ebcbaaee554dfec42ac8
Mirthiya <ragavanmirthiya@gmail.com>
2026-03-16 19:02:43 +0530

    Refactor blog scraper to use newspaper3k library

--- scraper/blog_scraper.py
+++ scraper/blog_scraper.py
@@ -1,48 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
+from newspaper import Article
 from langdetect import detect
 
 def scrape_blog(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
 
-    # Title
-    title = soup.title.text if soup.title else ""
+    article = Article(url)
 
-    # Author
-    author = ""
-    author_tag = soup.find("meta", {"name": "author"})
-    if author_tag:
-        author = author_tag.get("content")
+    article.download()
+    article.parse()
 
-    # Published date
-    published_date = ""
-    date_tag = soup.find("meta", {"property": "article:published_time"})
-    if date_tag:
-        published_date = date_tag.get("content")
+    title = article.title
+    authors = article.authors
+    publish_date = article.publish_date
+    text = article.text
 
-    # Extract paragraphs
-    paragraphs = soup.find_all("p")
-    content = [p.get_text() for p in paragraphs if len(p.get_text()) > 50]
+    paragraphs = text.split("\n")
 
-    full_text = " ".join(content)
-
-    # Language detection
+    language = "unknown"
     try:
-        language = detect(full_text)
+        language = detect(text)
     except:
-        language = "unknown"
+        pass
 
     data = {
         "source_url": url,
         "source_type": "blog",
-        "author": author,
-        "published_date": published_date,
+        "author": ", ".join(authors) if authors else "",
+        "published_date": str(publish_date) if publish_date else "",
         "language": language,
         "region": "",
         "topic_tags": [],
         "trust_score": "",
-        "content_chunks": content
+        "content_chunks": paragraphs
     }
 
     return data
