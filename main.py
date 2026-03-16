96c56bf45fc0156b6f3467811bca0893260756a0
Mirthiya <ragavanmirthiya@gmail.com>
2026-03-16 18:28:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+from scraper.blog_scraper import scrape_blog
+from scraper.youtube_scraper import scrape_youtube
+from scraper.pubmed_scraper import scrape_pubmed
+
+data = []
+
+# Example blog sources
+blogs = [
+    "https://towardsdatascience.com/",
+    "https://machinelearningmastery.com/",
+    "https://ai.googleblog.com/"
+]
+
+for blog in blogs:
+    try:
+        result = scrape_blog(blog)
+        data.append(result)
+    except:
+        pass
+
+# YouTube videos
+videos = [
+    "dQw4w9WgXcQ",
+    "3fumBcKC6RE"
+]
+
+for vid in videos:
+    try:
+        result = scrape_youtube(vid)
+        data.append(result)
+    except:
+        pass
+
+# PubMed article
+pubmed_id = "31452104"
+
+try:
+    result = scrape_pubmed(pubmed_id)
+    data.append(result)
+except:
+    pass
+
+
+with open("output/scraped_data.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+print("Scraping completed.")
