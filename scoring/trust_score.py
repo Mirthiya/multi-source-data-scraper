2d7a01c7e778bf2ef3191f0db528e58aaf633a8c
Mirthiya <ragavanmirthiya@gmail.com>
2026-03-16 18:15:30 +0530

    Add files via upload

--- scoring/trust_score.py
+++ scoring/trust_score.py
@@ -1,1 +1,13 @@
+def calculate_trust_score(author_score, citation_score,
+                          domain_score, recency_score,
+                          disclaimer_score):
 
+    trust_score = (
+        0.25 * author_score +
+        0.20 * citation_score +
+        0.20 * domain_score +
+        0.20 * recency_score +
+        0.15 * disclaimer_score
+    )
+
+    return round(trust_score, 2)
