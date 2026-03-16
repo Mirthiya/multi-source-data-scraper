fa282c7a8458ef54aafc574cd9fa27a91db997e1
Mirthiya <ragavanmirthiya@gmail.com>
2026-03-16 19:04:34 +0530

    Refactor scrape_youtube function to simplify return

--- scraper/youtube_scraper.py
+++ scraper/youtube_scraper.py
@@ -3,13 +3,14 @@ from youtube_transcript_api import YouTubeTranscriptApi
 def scrape_youtube(video_id):
 
     transcript_text = []
+
     try:
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
         transcript_text = [t["text"] for t in transcript]
     except:
         transcript_text = []
 
-    data = {
+    return {
         "source_url": f"https://youtube.com/watch?v={video_id}",
         "source_type": "youtube",
         "author": "",
@@ -20,5 +21,3 @@ def scrape_youtube(video_id):
         "trust_score": "",
         "content_chunks": transcript_text
     }
-
-    return data
