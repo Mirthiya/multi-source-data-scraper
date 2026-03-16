2d465e1186014f6c4f8fa3f77aed07d76cb42409
Mirthiya <ragavanmirthiya@gmail.com>
2026-03-16 18:30:39 +0530

    Refactor chunk_text to handle paragraphs instead of words

--- utils/chunking.py
+++ utils/chunking.py
@@ -1,9 +1,9 @@
-def chunk_text(text, size=200):
-    words = text.split()
+def chunk_text(paragraphs, chunk_size=3):
+
     chunks = []
 
-    for i in range(0, len(words), size):
-        chunk = " ".join(words[i:i+size])
+    for i in range(0, len(paragraphs), chunk_size):
+        chunk = " ".join(paragraphs[i:i+chunk_size])
         chunks.append(chunk)
 
     return chunks
