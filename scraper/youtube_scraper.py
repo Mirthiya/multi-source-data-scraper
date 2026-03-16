1dbd4b663b7e03fc32c6721f5cd80204a35100d9
Mirthiya <ragavanmirthiya@gmail.com>
2026-03-16 19:25:31 +0530

    Update youtube_scraper.py

--- scraper/youtube_scraper.py
+++ scraper/youtube_scraper.py
@@ -1,21 +1,31 @@
+from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def scrape_youtube(video_id):
 
+    url = f"https://youtube.com/watch?v={video_id}"
+
+    yt = YouTube(url)
+
+    channel = yt.author
+    publish_date = str(yt.publish_date)
+    description = yt.description
+
+    # Try to get transcript
     transcript_text = []
 
     try:
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
         transcript_text = [t["text"] for t in transcript]
     except:
-        transcript_text = []
+        transcript_text = description.split("\n")
 
     return {
-        "source_url": f"https://youtube.com/watch?v={video_id}",
+        "source_url": url,
         "source_type": "youtube",
-        "author": "",
-        "published_date": "",
-        "language": "",
+        "author": channel,
+        "published_date": publish_date,
+        "language": "en",
         "region": "",
         "topic_tags": [],
         "trust_score": "",
